36786ade0c4d98705e8ec800ab136a2285e9fac1
Kim <48668211+SkydevilK@users.noreply.github.com>
2020-07-11 17:42:36 +0900

    Update 3번.cpp

--- Kakao/2020_Internship/3번.cpp
+++ Kakao/2020_Internship/3번.cpp
@@ -67,9 +67,8 @@ int main() {
 	v = solution({ "ZZZ", "YYY", "NNNN", "YYY", "BBB" });
 	cout << v[0] << " " << v[1] << endl;
 }
-/*
-["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]	[3, 7]
-["AA", "AB", "AC", "AA", "AC"]	[1, 3]
-["XYZ", "XYZ", "XYZ"]	[1, 1]
-["ZZZ", "YYY", "NNNN", "YYY", "BBB"]	[1, 5]
-*/
+//["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]	[3, 7]
+//["AA", "AB", "AC", "AA", "AC"]	[1, 3]
+//["XYZ", "XYZ", "XYZ"]	[1, 1]
+//["ZZZ", "YYY", "NNNN", "YYY", "BBB"]	[1, 5]
+
